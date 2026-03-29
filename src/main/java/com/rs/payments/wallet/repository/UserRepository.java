49cb49c5b7b74ba38712e0bc7aa4f499e2c034cf
septopus9 <septopus9#gmail.com>
2026-03-29 16:38:23 +0530

    Add Two methods in UserRepository

--- src/main/java/com/rs/payments/wallet/repository/UserRepository.java
+++ src/main/java/com/rs/payments/wallet/repository/UserRepository.java
@@ -1,8 +1,12 @@
 package com.rs.payments.wallet.repository;
 
-import java.util.UUID;
 import com.rs.payments.wallet.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.UUID;
+
 public interface UserRepository extends JpaRepository<User, UUID> {
+
+    boolean existsByUsername(String username);
+    boolean existsByEmail(String email);
 }
